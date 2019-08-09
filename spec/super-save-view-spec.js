9993bebf1e47689e7787146a8e884bbc0e690691
rohaldbUni <z5019999@ad.unsw.edu.au>
2019-08-09 18:10:38 +1000

    basic version working

---
+++ spec/super-save-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SuperSaveView from '../lib/super-save-view';
+
+describe('SuperSaveView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
